23a801a71cc66d844cb1c44f3e57d7082493adf1
Jabed <jabedhosenn@gmail.com>
2024-06-25 10:47:17 +0600

    Module - 20 & 21

---
+++ Module - 21 - Bank Management System P1/mamar_bank/accounts/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+from .forms import UserRegistrationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+# Create your views here.
+class UserRegistrationView(FormView):
+    template_name = "accounts/user_registration.html"
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy("register")
+
+    def form_valid(self, form):
+        user = form.save()
+        login(user)
+        return super().form_valid(form)
+
+
+class UserLoginView(LoginView):
+    template_name = "accounts/user_login.html"
+
+    def get_success_url(self):
+        return reverse_lazy("home")
+
+
+class UserLogoutView(LogoutView):
+    def get_success_url(self):
+        if self.request.user.is_authenticated:
+            logout(self.request)
+        return reverse_lazy("home")
